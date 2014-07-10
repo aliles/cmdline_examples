5125e40989e3ba1c364e67ba6e675c354383872c
Aaron Iles <aaron.iles@gmail.com>
2014-07-10 19:51:33 +1000

    Fix PEP8 violations
    Missing blank lines between function definitions added.

--- tw_begins.py
+++ tw_begins.py
@@ -12,17 +12,20 @@ def timeline():
     for status in begin.context.api.timeline:
         print u"%s: %s" % (status.user.screen_name, status.text)
 
+
 @begin.subcommand
 def mentions():
     "Display recent tweets mentioning user"
     for status in begin.context.api.mentions:
         print u"%s: %s" % (status.user.screen_name, status.text)
 
+
 @begin.subcommand
 def retweets():
     "Display recent retweets from user's timeline"
     for status in begin.context.api.retweets:
         print u"%s: %s" % (status.user.screen_name, status.text)
+
 
 # Program main definition replace __name__ === '__main__' magic
 # sub-commands are registered and loaded automatically.
