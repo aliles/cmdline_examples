b92b4a7093192b70dbdff95441a54425272ce390
Aaron Iles <aaron.iles@gmail.com>
2014-07-12 22:58:42 +1000

    Fix PEP8 violations
    Mising blank lines between function defintions added. Lines greather
    than 80 characters continued on additional lines.

--- tw_optparse.py
+++ tw_optparse.py
@@ -4,15 +4,21 @@ import os
 
 import twitterlib
 
+
 # Process command line using OptionParser.
 def parse_commandline(args=None):
     "Process command line using optparse"
     parser = OptionParser()
-    parser.add_option("--api-key", default=os.environ.get('API_KEY', None))
-    parser.add_option("--api-secret", default=os.environ.get('API_SECRET', None))
-    parser.add_option("--access-token", default=os.environ.get('ACCESS_TOKEN', None))
-    parser.add_option("--access-secret", default=os.environ.get('ACCESS_SECRET', None))
+    parser.add_option("--api-key",
+                      default=os.environ.get('API_KEY', None))
+    parser.add_option("--api-secret",
+                      default=os.environ.get('API_SECRET', None))
+    parser.add_option("--access-token",
+                      default=os.environ.get('ACCESS_TOKEN', None))
+    parser.add_option("--access-secret",
+                      default=os.environ.get('ACCESS_SECRET', None))
     return parser.parse_args(args)
+
 
 # Sub-commands for select Twitter feeds.
 def timeline(api):
@@ -20,15 +26,18 @@ def timeline(api):
     for status in api.timeline:
         print u"%s: %s" % (status.user.screen_name, status.text)
 
+
 def mentions(api):
     "Display recent tweets mentioning user"
     for status in api.mentions:
         print u"%s: %s" % (status.user.screen_name, status.text)
 
+
 def retweets(api):
     "Display recent retweets from user's timeline"
     for status in api.retweets:
         print u"%s: %s" % (status.user.screen_name, status.text)
+
 
 # Maps sub-command names to function calls.
 SUB_COMMANDS = {
