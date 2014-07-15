e18628f08ddac1fa6ef9cb4b8e690a90d4c21265
Aaron Iles <aaron.iles@gmail.com>
2014-07-15 22:40:42 +1000

    Fix PEP8 violations
    Line continuations correctly aligned with start of implied block.
    Missing blank lines between fucntion definitions added.

--- tw_argparse.py
+++ tw_argparse.py
@@ -4,53 +4,54 @@ import os
 
 import twitterlib
 
+
 # Sub-commands for select Twitter feeds.
 def timeline(args):
     "Display recent tweets from users timeline"
     api = twitterlib.API(args.api_key,
-        args.api_secret,
-        args.access_token,
-        args.access_secret
-    )
+                         args.api_secret,
+                         args.access_token,
+                         args.access_secret)
     for status in api.timeline:
         print u"%s: %s" % (status.user.screen_name, status.text)
+
 
 def mentions(args):
     "Display recent tweets mentioning user"
     api = twitterlib.API(args.api_key,
-        args.api_secret,
-        args.access_token,
-        args.access_secret
-    )
+                         args.api_secret,
+                         args.access_token,
+                         args.access_secret)
     for status in api.mentions:
         print u"%s: %s" % (status.user.screen_name, status.text)
+
 
 def retweets(args):
     "Display recent retweets from user's timeline"
     api = twitterlib.API(args.api_key,
-        args.api_secret,
-        args.access_token,
-        args.access_secret
-    )
+                         args.api_secret,
+                         args.access_token,
+                         args.access_secret)
     for status in api.retweets:
         print u"%s: %s" % (status.user.screen_name, status.text)
+
 
 # Process command line using argparse.
 def parse_commandline(args=None):
     "Process command line using optparse"
     parser = argparse.ArgumentParser()
     parser.add_argument("--api-key",
-            required='API_KEY' not in os.environ,
-            default=os.environ.get('API_KEY', None))
+                        required='API_KEY' not in os.environ,
+                        default=os.environ.get('API_KEY', None))
     parser.add_argument("--api-secret",
-            required='API_SECRET' not in os.environ,
-            default=os.environ.get('API_SECRET', None))
+                        required='API_SECRET' not in os.environ,
+                        default=os.environ.get('API_SECRET', None))
     parser.add_argument("--access-token",
-            required='ACCESS_TOKEN' not in os.environ,
-            default=os.environ.get('ACCESS_TOKEN', None))
+                        required='ACCESS_TOKEN' not in os.environ,
+                        default=os.environ.get('ACCESS_TOKEN', None))
     parser.add_argument("--access-secret",
-            required='ACCESS_SECRET' not in os.environ,
-            default=os.environ.get('ACCESS_SECRET', None))
+                        required='ACCESS_SECRET' not in os.environ,
+                        default=os.environ.get('ACCESS_SECRET', None))
     # Create sub-parser and register sub-command funciton.
     subparsers = parser.add_subparsers(dest='sub_command')
     subparsers.add_parser('timeline').set_defaults(func=timeline)
